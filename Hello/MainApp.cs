b10c87d7f5051938881b56f641fbf5742d105dd0
dazzleGames <stellagalaxy39@protonmail.com>
2026-09-01 10:26:23 +0900

    초기 커밋.

---
+++ Hello/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp //프로그램이 시작 되는 곳
+    {
+        static void Main(string[] arge)
+        {
+            if (arge.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+            WriteLine("Hello, {0}!", arge[0]);
+        }
+    }
+}
